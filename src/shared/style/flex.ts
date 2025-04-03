9a10b0433bf5cf4ab1691b75bba4f9db2b319d76
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-04-03 09:25:47 +0900

    개발세팅

---
+++ src/shared/style/flex.ts
@@ -0,0 +1,20 @@
+const theme = {
+  white: "#FFFFFF",
+  black: "#000000",
+  primaryPink: "#FF8491",
+  resultPink: "#FCEBF1",
+  resultYellow: "#FDF6EF",
+
+  brown1: "#EEEDED",
+  brown2: "#D6D3D3",
+  brown3: "#B6B0AF",
+  brown4: "#958B8A",
+  brown5: "#746867",
+  brown6: "#564746",
+  brown7: "#493C3C",
+  brown8: "#3D3232",
+  brown9: "#312828",
+  grey10: "#27201F",
+};
+
+export default theme;
