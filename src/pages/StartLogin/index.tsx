fc34085f70fb2cb0cd70c9887a4dfd5b71547737
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-04-07 07:54:46 +0900

    chore: 불필요한 공백 제거

--- src/pages/StartLogin/index.tsx
+++ src/pages/StartLogin/index.tsx
@@ -17,16 +17,13 @@ const StartLogin = () => {
         <span>그 말, 무슨 의미일까?</span>
         <span>티나가 알려줄게요</span>
       </S.TextContainer>
-
       <S.LoginContainer>
         <S.LoginCenter>
           <Kakao />
           <S.Login>카카오 로그인</S.Login>
         </S.LoginCenter>
       </S.LoginContainer>
-
       <S.NoLogin onClick={handleNoLoginClick}>로그인 없이 진행하기</S.NoLogin>
-
       {showModal && (
         <S.ModalOverlay onClick={handleCloseModal}>
           <div onClick={(e) => e.stopPropagation()}>
