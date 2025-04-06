12002fdef2fc318182daa1e311f8657b9aaacfaa
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-04-07 00:46:41 +0900

    feat: 엠비티아이 결과 컴포넌트 개발

---
+++ src/components/MbtiResultChar/index.tsx
@@ -0,0 +1,59 @@
+import * as S from "./style";
+import ENFJ from "../../assets/illustration/ENFJ.png";
+import ENFP from "../../assets/illustration/ENFP.png";
+import ENTJ from "../../assets/illustration/ENTJ.png";
+import ENTP from "../../assets/illustration/ENTP.png";
+import ESFJ from "../../assets/illustration/ESFJ.png";
+import ESFP from "../../assets/illustration/ESFP.png";
+import ESTJ from "../../assets/illustration/ESTJ.png";
+import ESTP from "../../assets/illustration/ESTP.png";
+import INFJ from "../../assets/illustration/INFJ.png";
+import INFP from "../../assets/illustration/INFP.png";
+import INTJ from "../../assets/illustration/INTJ.png";
+import INTP from "../../assets/illustration/INTP.png";
+import ISFJ from "../../assets/illustration/ISFJ.png";
+import ISFP from "../../assets/illustration/ISFP.png";
+import ISTJ from "../../assets/illustration/ISTJ.png";
+import ISTP from "../../assets/illustration/ISTP.png";
+
+interface MbtiResultCharProps {
+  isMine: boolean;
+  mbti: string;
+}
+
+const mbtiImageMap: Record<string, string> = {
+  ENFJ,
+  ENFP,
+  ENTJ,
+  ENTP,
+  ESFJ,
+  ESFP,
+  ESTJ,
+  ESTP,
+  INFJ,
+  INFP,
+  INTJ,
+  INTP,
+  ISFJ,
+  ISFP,
+  ISTJ,
+  ISTP,
+};
+
+const MbtiResultChar = ({ isMine, mbti }: MbtiResultCharProps) => {
+  return (
+    <S.Layout>
+      <S.CharacterWrapper>
+        <S.ImgBackGround />
+        <S.MbtiCharacter src={mbtiImageMap[mbti]} />
+      </S.CharacterWrapper>
+
+      <S.UserDetail>
+        {isMine ? <S.User>나</S.User> : <S.User>상대</S.User>}
+        <S.Mbti>({mbti})</S.Mbti>
+      </S.UserDetail>
+    </S.Layout>
+  );
+};
+
+export default MbtiResultChar;
---
+++ src/components/MbtiResultChar/style.ts
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import flex from "../../shared/style/flex";
+import theme from "../../shared/style/theme";
+
+export const Layout = styled.main`
+  ${flex.COLUMN_CENTER}
+`;
+
+export const CharacterWrapper = styled.div`
+  position: relative;
+  width: 140px;
+  height: 140px;
+  ${flex.CENTER}
+`;
+
+export const ImgBackGround = styled.div`
+  position: relative;
+  width: 140px;
+  height: 140px;
+  border-radius: 1000px;
+  background: linear-gradient(
+    180deg,
+    rgba(255, 132, 145, 0.16) 0%,
+    rgba(255, 132, 145, 0.03) 100%
+  );
+  filter: blur(6px);
+  z-index: 0;
+`;
+
+export const MbtiCharacter = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+`;
+
+export const UserDetail = styled.div`
+  ${flex.CENTER}
+  gap: 4px;
+`;
+
+export const User = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  color: ${theme.brown5};
+`;
+
+export const Mbti = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  color: ${theme.brown4};
+`;
