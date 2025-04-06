20afdf3e09a80c5636a9c6c583924958371c8f5c
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-04-06 22:35:22 +0900

    feat: 모달 컴포넌트 개발

---
+++ src/components/NoticeModal/index.tsx
@@ -0,0 +1,50 @@
+import * as S from "./style";
+
+interface NoticeModalProps {
+  name?: string;
+  isResult: boolean;
+}
+
+const NoticeModal = ({ name, isResult }: NoticeModalProps) => {
+  const ModalDetail = [
+    {
+      id: 1,
+      mainText: `${name}님의 분석결과를 <br> 삭제할까요?`,
+      subText: "삭제하면 복구할 수 없어요",
+      pinkBtnText: "네",
+      blackBtnText: "아니요",
+    },
+    {
+      id: 2,
+      mainText: "주의사항",
+      subText: (
+        <>
+          로그인을 진행하지 않으면 <br />
+          분석 결과를 <S.Highlight>저장할 수 없습니다</S.Highlight>
+        </>
+      ),
+      pinkBtnText: "로그인",
+      blackBtnText: "괜찮아요",
+    },
+  ];
+
+  const { mainText, subText, pinkBtnText, blackBtnText } = isResult
+    ? ModalDetail[0]
+    : ModalDetail[1];
+
+  return (
+    <S.Layout>
+      <S.TextContainer isResult={isResult}>
+        <S.MainText dangerouslySetInnerHTML={{ __html: mainText }} />
+        <S.SubText>{subText}</S.SubText>
+      </S.TextContainer>
+      <S.ButtonContainer>
+        <S.PinkBtn>{pinkBtnText}</S.PinkBtn>
+        <S.Line />
+        <S.BlackBtn>{blackBtnText}</S.BlackBtn>
+      </S.ButtonContainer>
+    </S.Layout>
+  );
+};
+
+export default NoticeModal;
---
+++ src/components/NoticeModal/style.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import flex from "../../shared/style/flex";
+import theme from "../../shared/style/theme";
+
+export const Layout = styled.main`
+  ${flex.COLUMN_CENTER}
+  background-color: rgba(255, 255, 255, 0.90);
+
+  border-radius: 12px;
+`;
+export const TextContainer = styled.div<{ isResult: boolean }>`
+  ${flex.COLUMN_CENTER}
+  gap: 16px;
+  text-align: center;
+  padding: ${({ isResult }) =>
+    isResult ? "24px 30px 19px 30px" : "24px 30px 26px 30px"};
+`;
+export const MainText = styled.span`
+  font-size: 18px;
+  font-weight: 600;
+  color: ${theme.brown9};
+`;
+export const SubText = styled.span`
+  font-size: 12px;
+  font-weight: 500;
+  color: ${theme.brown8};
+`;
+export const ButtonContainer = styled.div`
+  ${flex.FLEX}
+  width: 100%;
+  border-top: 1px solid ${theme.white};
+  font-size: 12px;
+  font-weight: 500;
+`;
+export const PinkBtn = styled.button`
+  outline: none;
+  border: none;
+  width: 50%;
+  padding: 11px 0;
+  border-radius: 0 0 0 12px;
+  color: ${theme.primaryPink};
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0.9);
+`;
+export const Line = styled.div`
+  border-left: 1px solid ${theme.white};
+`;
+export const BlackBtn = styled.button`
+  outline: none;
+  border: none;
+  width: 50%;
+  padding: 11px 0;
+  border-radius: 0 0 12px 0;
+  color: ${theme.brown4};
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0.9);
+`;
+export const Highlight = styled.span`
+  color: ${theme.primaryPink};
+`;
