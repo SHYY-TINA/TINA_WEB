79175c8538ae57d80e44029524e4ba0519532e5e
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-04-07 17:25:46 +0900

    feat: 상대 분석 결과 페이지 퍼블리싱

---
+++ src/pages/OtherResult/index.tsx
@@ -0,0 +1,84 @@
+import * as S from "./style";
+import LeftArrow from "../../assets/icons/leftArrow";
+import Arrow from "../../assets/icons/arrow";
+import Heart from "../../assets/icons/heart";
+import ChatBox from "../../components/ChatBox";
+
+interface OtherResultProps {
+  myName: string;
+  otherName: string;
+}
+
+const OtherResult = ({ myName, otherName }: OtherResultProps) => {
+  return (
+    <S.Layout>
+      <S.Header>
+        <S.HighHeader>
+          <div style={{ position: "absolute", left: 0 }}>
+            <LeftArrow />
+          </div>
+          <S.FromContainer>
+            {myName}
+            <Arrow />
+            {otherName}
+          </S.FromContainer>
+        </S.HighHeader>
+        <S.LowHeader>
+          <S.HeartContainer>
+            <Heart />
+            <span>98</span>%
+          </S.HeartContainer>
+        </S.LowHeader>
+      </S.Header>
+      <S.ChatContainer>
+        <ChatBox
+          chatData="모든 국민은 보건에 관하여 국가의 보호를 받는다."
+          date="오전 12시"
+          isFirstChat
+          isMyChat={false}
+        />
+        <ChatBox
+          chatData="모든 국민은 보건에 관하여 국가의 보호를 받는다."
+          date="오전 12시"
+          isFirstChat={false}
+          isMyChat={false}
+        />
+      </S.ChatContainer>
+      <S.Main>
+        <S.ResultContainer>
+          <S.Title>호감은 있는데 아직 잘 모르겠어요</S.Title>
+          <S.ResultP>
+            공무원의 직무상 불법행위로 손해를 받은 국민은 법률이 정하는 바에
+            의하여 국가 또는 공공단체에 정당한 배상을 청구할 수 있다. 이 경우
+            공무원 자신의 책임은 면제되지 아니한다. 대통령은 필요하다고 인정할
+            때에는 외교·국방·통일 기타 국가안위에 관한 중요정책을 국민투표에
+            붙일 수 있다.{" "}
+          </S.ResultP>
+        </S.ResultContainer>
+        <S.ResultContainer>
+          <S.Title>대화 TIP</S.Title>
+          <S.ResultY>
+            <S.TipResult>상대의 말에 진심으로 공감해 주세요</S.TipResult>
+            <S.TipResult>상대는 칭찬해 주는 것을 좋아해요</S.TipResult>
+          </S.ResultY>
+        </S.ResultContainer>
+        <S.ResultContainer>
+          <S.TextContainer>
+            <S.Title>주의</S.Title>
+            <S.SubText>이 행동이 바뀌지 않으면 차일 가능성 65%</S.SubText>
+          </S.TextContainer>
+          <S.ResultP>
+            <S.TipResult>상대의 말에 진심으로 공감해 주세요</S.TipResult>
+            <S.TipResult>상대는 칭찬해 주는 것을 좋아해요</S.TipResult>
+          </S.ResultP>
+        </S.ResultContainer>
+        <S.BtnContainer>
+          <S.SaveBtn>저장하기</S.SaveBtn>
+          <S.More>속마음 더 보기</S.More>
+        </S.BtnContainer>
+      </S.Main>
+    </S.Layout>
+  );
+};
+
+export default OtherResult;
---
+++ src/pages/OtherResult/style.ts
@@ -0,0 +1,143 @@
+import styled from "styled-components";
+import theme from "../../shared/style/theme";
+import flex from "../../shared/style/flex";
+
+export const Layout = styled.main`
+  ${flex.COLUMN_FLEX}
+  width: 100vw;
+  height: 100vh;
+  background: radial-gradient(
+      60.13% 15.23% at 50.13% 21.53%,
+      #fec9d0 0%,
+      #fef6f3 100%
+    ),
+    #fff;
+`;
+export const Header = styled.header`
+  ${flex.COLUMN_FLEX}
+  width: 100%;
+  padding: 53px 20px 7px 20px;
+  gap: 7px;
+  border-radius: 0 0 16px 16px;
+  background-color: ${theme.white};
+`;
+export const HighHeader = styled.div`
+  ${flex.CENTER}
+  position: relative;
+  width: 100%;
+`;
+export const FromContainer = styled.div`
+  ${flex.CENTER}
+  color: ${theme.brown9};
+  font-size: 14px;
+  font-weight: 400;
+`;
+export const LowHeader = styled.div`
+  width: 100%;
+  ${flex.CENTER}
+`;
+export const HeartContainer = styled.div`
+  ${flex.CENTER}
+  font-size: 12px;
+  color: #ab6e6b;
+  font-weight: 500;
+`;
+export const ChatContainer = styled.div`
+  ${flex.COLUMN_FLEX}
+  padding: 32px 20px;
+  gap: 4px;
+`;
+export const Main = styled.div`
+  ${flex.COLUMN_CENTER}
+  padding: 42px 18px 50px 19px;
+  background-color: ${theme.white};
+  border-radius: 16px 16px 0 0;
+  gap: 32px;
+`;
+export const ResultContainer = styled.div`
+  ${flex.COLUMN_CENTER}
+  width: 100%;
+  gap: 12px;
+`;
+export const Title = styled.span`
+  color: ${theme.brown6};
+  font-size: 18px;
+  font-weight: 600;
+`;
+export const ResultP = styled.div`
+  ${flex.COLUMN_FLEX}
+  width: 100%;
+  background-color: ${theme.resultPink};
+  border-radius: 16px;
+  padding: 12px 19px;
+  color: ${theme.brown7};
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 160%;
+`;
+export const ResultY = styled.div`
+  ${flex.COLUMN_FLEX}
+  width: 100%;
+  background-color: ${theme.resultYellow};
+  border-radius: 16px;
+  padding: 12px 19px;
+  color: ${theme.brown7};
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 160%;
+`;
+export const TipResult = styled.span`
+  width: 100%;
+  position: relative;
+  padding-left: 20px;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 160%;
+  color: ${theme.brown7};
+
+  &::before {
+    content: "•";
+    position: absolute;
+    left: 0;
+    top: 0;
+    font-size: 16px;
+    line-height: 160%;
+  }
+`;
+export const TextContainer = styled.div`
+  ${flex.COLUMN_CENTER}
+  gap: 8px;
+`;
+export const SubText = styled.span`
+  color: ${theme.brown6};
+  font-size: 12px;
+  font-weight: 400;
+`;
+export const BtnContainer = styled.div`
+  ${flex.COLUMN_CENTER}
+  gap: 12px;
+  width: 191px;
+`;
+export const SaveBtn = styled.button`
+  width: 100%;
+  padding: 14px 0 13px 0;
+  border: 1px solid ${theme.primaryPink};
+  border-radius: 12px;
+  color: ${theme.primaryPink};
+  background-color: ${theme.white};
+  font-size: 18px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+export const More = styled.button`
+  width: 100%;
+  padding: 14px 0 13px 0;
+  border: 1px solid ${theme.primaryPink};
+  border-radius: 12px;
+  color: ${theme.white};
+  background-color: ${theme.primaryPink};
+  font-size: 18px;
+  font-weight: 600;
+  cursor: pointer;
+`;
