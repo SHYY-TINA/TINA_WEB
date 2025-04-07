f4a2ca65a7b3025f7292d3c98e71728dc677693c
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-04-07 22:54:50 +0900

    feat: 프로필 수정 페이지 퍼블리싱 + 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import StartLogin from "./pages/StartLogin";
+import InputUserDetail from "./pages/InputUserDetail";
+import Home from "./pages/Home";
+import InputOtherDetail from "./pages/InputOtherDetail";
+import UploadFile from "./pages/UploadFile";
+import AnalysisResult from "./pages/AnalysisResult";
+import ResultDetail from "./pages/ResultDetail";
+import GatherResult from "./pages/GatherResult";
+import Mbti from "./pages/Mbti";
+import MbtiResult from "./pages/MbtiResult";
+import UserProfileEdit from "./pages/UserProfileEdit";
+
 function App() {
   return (
-    <>
-      <div>app page</div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<StartLogin />} />
+        <Route path="/input-user-detail" element={<InputUserDetail />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/input-other-detail" element={<InputOtherDetail />} />
+        <Route path="/upload-file" element={<UploadFile />} />
+        <Route path="/other-result" element={<AnalysisResult isOther />} />
+        <Route path="/my-result" element={<AnalysisResult />} />
+        <Route path="/detail-result" element={<ResultDetail />} />
+        <Route path="/gather-result" element={<GatherResult />} />
+        <Route path="/match-mbti" element={<Mbti />} />
+        <Route path="/mbti-result" element={<MbtiResult heartNum={4} />} />
+        <Route
+          path="/profile-edit"
+          element={
+            <UserProfileEdit userMbti="ISTP" userName="예진" jender="여성" />
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/UserProfileEdit/index.tsx
@@ -0,0 +1,107 @@
+import { useState, useEffect } from "react";
+import Button from "../../components/Button";
+import Input from "../../components/input";
+import * as S from "./style";
+import { useNavigate } from "react-router-dom";
+import LeftArrow from "../../assets/icons/leftArrow";
+
+interface ProfileEditProps {
+  userName: string;
+  userMbti: string;
+  jender: string;
+}
+
+const UserProfileEdit = ({ userName, userMbti, jender }: ProfileEditProps) => {
+  // Set default values from props
+  const [name, setName] = useState(userName);
+  const [mbti, setMbti] = useState(userMbti);
+  const [gender, setGender] = useState(jender);
+
+  // Track if any input value is changed
+  const [isModified, setIsModified] = useState(false);
+
+  // Check if any input value is different from the default value
+  useEffect(() => {
+    setIsModified(name !== userName || mbti !== userMbti || gender !== jender);
+  }, [name, mbti, gender, userName, userMbti, jender]);
+
+  const isValid = name && mbti && gender;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (isValid && isModified) {
+      navigate("/upload-file");
+    }
+  };
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <S.Layout>
+      <S.Header>
+        <div onClick={goBack}>
+          <LeftArrow />
+        </div>
+      </S.Header>
+      <S.Title>프로필 수정</S.Title>
+      <S.SubTitleContainer>
+        달라진 정보가 있다면 사용해 주세요!
+      </S.SubTitleContainer>
+      <S.InputContainer>
+        <S.Container>
+          <S.InputTitle>이름</S.InputTitle>
+          <Input
+            placeholder="카카오톡에 저장된 이름을 입력해 주세요  ex)예진"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </S.Container>
+        <S.Container>
+          <S.InputTitle>MBTI</S.InputTitle>
+          <Input
+            placeholder="사용자의 MBTI를 알려주세요  ex)ISTJ"
+            value={mbti}
+            onChange={(e) => setMbti(e.target.value)}
+          />
+        </S.Container>
+        <S.Container>
+          <S.InputTitle>성별</S.InputTitle>
+          <S.FlexContainer>
+            <S.JenderInputBox>
+              <S.Radio
+                type="radio"
+                name="gender"
+                value="남성"
+                checked={gender === "남성"}
+                onChange={(e) => setGender(e.target.value)}
+              />
+              남성
+            </S.JenderInputBox>
+            <S.JenderInputBox>
+              <S.Radio
+                type="radio"
+                name="gender"
+                value="여성"
+                checked={gender === "여성"}
+                onChange={(e) => setGender(e.target.value)}
+              />
+              여성
+            </S.JenderInputBox>
+          </S.FlexContainer>
+        </S.Container>
+      </S.InputContainer>
+      <S.BtnContainer>
+        <Button
+          mode="BIGPINK"
+          text="수정하기"
+          isActive={isModified && isValid} // Only active if values are modified and valid
+          onClick={handleClick}
+        />
+      </S.BtnContainer>
+    </S.Layout>
+  );
+};
+
+export default UserProfileEdit;
