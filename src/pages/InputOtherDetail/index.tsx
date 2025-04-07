3e19eefece9abdf43325c09f06d7fe27c585534a
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-04-07 11:23:38 +0900

    feat: 상대방 정보 입력 페이지 퍼블리싱 + 라우팅 설정

---
+++ src/pages/InputOtherDetail/index.tsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import Button from "../../components/Button";
+import Input from "../../components/input";
+import * as S from "./style";
+import { useNavigate } from "react-router-dom";
+import LeftArrow from "../../assets/icons/leftArrow";
+
+const InputOtherDetail = () => {
+  const [name, setName] = useState("");
+  const [mbti, setMbti] = useState("");
+  const [gender, setGender] = useState("");
+
+  const isValid = name && mbti && gender;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    if (isValid) {
+      navigate("/home");
+    }
+  };
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <S.Layout>
+      <S.Header>
+        <div onClick={goBack}>
+          <LeftArrow />
+        </div>
+      </S.Header>
+      <S.Title>상대방 정보 입력</S.Title>
+      <S.SubTitleContainer>
+        알려주신 정보는 정확한 분석을 위해 사용해요
+      </S.SubTitleContainer>
+      <S.InputContainer>
+        <S.Container>
+          <S.InputTitle>이름</S.InputTitle>
+          <Input
+            placeholder="카카오톡에 저장된 이름을 입력해 주세요  ex)예진"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </S.Container>
+        <S.Container>
+          <S.InputTitle>MBTI</S.InputTitle>
+          <Input
+            placeholder="사용자의 MBTI를 알려주세요  ex)ISTJ"
+            value={mbti}
+            onChange={(e) => setMbti(e.target.value)}
+          />
+        </S.Container>
+        <S.Container>
+          <S.InputTitle>성별</S.InputTitle>
+          <S.FlexContainer>
+            <S.JenderInputBox>
+              <S.Radio
+                type="radio"
+                name="gender"
+                value="남성"
+                checked={gender === "남성"}
+                onChange={(e) => setGender(e.target.value)}
+              />
+              남성
+            </S.JenderInputBox>
+            <S.JenderInputBox>
+              <S.Radio
+                type="radio"
+                name="gender"
+                value="여성"
+                checked={gender === "여성"}
+                onChange={(e) => setGender(e.target.value)}
+              />
+              여성
+            </S.JenderInputBox>
+          </S.FlexContainer>
+        </S.Container>
+      </S.InputContainer>
+      <S.BtnContainer>
+        <Button
+          mode="BIGPINK"
+          text="완료하기"
+          isActive={!!isValid}
+          onClick={handleClick}
+        />
+      </S.BtnContainer>
+    </S.Layout>
+  );
+};
+
+export default InputOtherDetail;
---
+++ src/pages/InputOtherDetail/style.ts
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import flex from "../../shared/style/flex";
+import theme from "../../shared/style/theme";
+
+export const Layout = styled.main`
+  ${flex.COLUMN_FLEX}
+  width: 100vw;
+  height: 100vh;
+  padding: 47px 20px 0 20px;
+  background: linear-gradient(
+      180deg,
+      rgba(255, 255, 255, 0.5) 57.26%,
+      rgba(252, 235, 241, 0.5) 99.97%
+    ),
+    #fff;
+`;
+export const Header = styled.header`
+  padding: 6px 0;
+  padding-bottom: 33px;
+`;
+export const Title = styled.span`
+  color: ${theme.brown8};
+  font-size: 20px;
+  font-weight: 600;
+`;
+export const SubTitleContainer = styled.span`
+  ${flex.COLUMN_FLEX}
+  padding-top: 4px;
+  color: ${theme.brown4};
+  font-size: 14px;
+  font-weight: 400;
+`;
+export const InputContainer = styled.div`
+  ${flex.COLUMN_FLEX}
+  padding: 12px 0 44px 0;
+  width: 100%;
+  gap: 10px;
+`;
+export const Container = styled.div`
+  ${flex.COLUMN_FLEX}
+  width: 100%;
+  gap: 2px;
+`;
+export const InputTitle = styled.span`
+  color: ${theme.brown6};
+  font-size: 14px;
+  font-weight: 400;
+`;
+export const FlexContainer = styled.div`
+  ${flex.FLEX}
+  margin-top: 1px;
+  gap: 16px;
+`;
+export const JenderInputBox = styled.div`
+  ${flex.CENTER}
+  gap: 4px;
+  color: ${theme.brown6};
+  font-size: 14px;
+  font-weight: 400;
+`;
+export const Radio = styled.input`
+  flex-shrink: 0;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  width: 12px;
+  height: 12px;
+  border: 1px solid ${theme.brown3};
+  border-radius: 50%;
+  outline: none;
+  cursor: pointer;
+  &:checked {
+    background-color: ${theme.primaryPink};
+    border: 3px solid #fff;
+    box-shadow: 0 0 0 1px ${theme.primaryPink};
+  }
+`;
+export const BtnContainer = styled.div`
+  ${flex.CENTER}
+  width: 100%;
+`;
