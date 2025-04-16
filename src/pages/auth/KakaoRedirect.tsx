3f1907394a66415760556adc0d42f60ff08e67fc
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-04-17 08:25:05 +0900

    feat: 로그인 api react-query 코드로 변경

--- src/pages/auth/KakaoRedirect.tsx
+++ src/pages/auth/KakaoRedirect.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLogin } from "@/shared/lib/useLogin";
+import { useAuthStore } from "@/shared/store/auth";
 
 const KakaoRedirect = () => {
   const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get("code");
+  const { setTokens } = useAuthStore();
 
   const { mutate, data, isPending, isError, error } = useLogin();
 
@@ -15,15 +17,21 @@ const KakaoRedirect = () => {
   }, [code, mutate]);
 
   useEffect(() => {
-    if (data?.tokenResponse?.accessToken) {
+    const access = data?.tokenResponse?.accessToken;
+    const refresh = data?.tokenResponse?.refreshToken;
+
+    if (access && refresh) {
+      setTokens(access, refresh);
+      localStorage.setItem("accessToken", access);
+      localStorage.setItem("refreshToken", refresh);
+
       const destination = data.detail ? "/home" : "/input-user-detail";
       navigate(destination);
     }
-  }, [data, navigate]);
+  }, [data, navigate, setTokens]);
 
   useEffect(() => {
     if (isError) {
-      console.error("로그인 실패:", error);
       alert("로그인 실패! 다시 시도해주세요.");
       navigate("/");
     }
