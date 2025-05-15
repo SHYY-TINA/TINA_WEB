163b8338cdad81b2763911f2629e4dc86663702c
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-05-15 17:49:11 +0900

    feat: 타입정리

--- src/shared/types/emotion.ts
+++ src/shared/types/emotion.ts
@@ -3,22 +3,50 @@ export interface ChatItem {
   meaning: string;
 }
 
+export interface EmotionRequest {
+  partnerName: string;
+  partnerMbti: string;
+  file: File;
+}
+
+export interface GuestEmotionResquest {
+  userName: string;
+  userMbti: string;
+  partnerName: string;
+  partnerMbti: string;
+  file: File;
+}
+
 export interface MyEmotionResponse {
+  userNickname: string;
+  partnerName: string;
+  partnerMbti: string;
   charmScore: number;
   feedbackTitle: string;
   feedbackContent: string;
   charmPointTitle: string;
-  charmPointContent: string;
-  chats: ChatItem[];
+  charmPointContent: string[];
+  chat: ChatItem[];
 }
 
 export interface OtherEmotionResponse {
+  userNickname: string;
+  partnerName: string;
+  partnerMbti: string;
   charmScore: number;
   feedbackTitle: string;
   feedbackContent: string;
   tipTitle: string;
-  tipContent: string;
+  tipContent: string[];
   cautionTitle: string;
-  cautionContent: string;
-  chats: ChatItem[];
+  cautionContent: string[];
+  chat: ChatItem[];
+}
+
+export interface EmotionRecord {
+  id: number;
+  userNickname: string;
+  partnerName: string;
+  partnerMbti: string;
+  charmScore: number;
 }
