336937e1a3aed43de959124e7e676bc792e35e27
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-05-15 17:48:57 +0900

    chore: auth관련 토큰 로컬 스토리지로 관리

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,12 +1,11 @@
 import { Navigate } from "react-router-dom";
-import { useAuthStore } from "@/shared/store/auth";
 
 interface ProtectedRouteProps {
   children: React.ReactElement;
 }
 
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-  const { accessToken } = useAuthStore();
+  const accessToken = localStorage.getItem("accessToken");
 
   if (!accessToken) {
     alert("로그인이 필요합니다!");
